c9752a765ec7a59e158feb23b273062ea7428d38
RWallan <3am.richardwallan@gmail.com>
2023-05-02 22:48:59 -0300

    ✨New features about process and tasks
    * Method to get process name
    * Method to start a process returning the task infos
    * Method to get task status

--- carol_app_monitor/carol_api.py
+++ carol_app_monitor/carol_api.py
@@ -1,12 +1,15 @@
 from typing import Any, Dict, List
 
 from dotenv import find_dotenv, load_dotenv
-from pycarol import Apps, Carol
+from pycarol import Apps, Carol, Tasks
+
+from carol_app_monitor.exceptions import TaskError
 
 _ = load_dotenv(find_dotenv(".env"))
 
 carol = Carol()
 apps = Apps(carol)
+tasks = Tasks(carol)
 
 
 def get_properties_from_app() -> Dict[str, Dict[str, Any]]:
@@ -64,3 +67,75 @@ def get_process_status(app_name: str | List[str]) -> Dict[str, str]:
         )
 
         return {app_name: status}
+
+
+def get_process_name(app_name: str | List[str]) -> List[Dict[str, str]]:
+    """Get process name.
+
+    Args:
+        app_name (str | List[str]): CarolApp name. Can be `str` or a `list`.
+
+    Returns:
+        List[Dict[str, str]]: A list of dicts with `app_name` and the\
+            `process_name`.
+    """
+    if isinstance(app_name, list):
+        process_names = [
+            apps.get_processes_info(app)
+            .get("mdmTenantAppAIProcessValues")[0]
+            .get("mdmName")
+            for app in app_name
+        ]
+
+        return [
+            {"app_name": app, "process_name": process_name}
+            for app, process_name in zip(app_name, process_names)
+        ]
+    else:
+        process_name = (
+            apps.get_processes_info(app_name)
+            .get("mdmTenantAppAIProcessValues")[0]
+            .get("mdmName")
+        )
+
+        return [{"app_name": app_name, "process_name": process_name}]
+
+
+def start_app_process(app_info: Dict[str, str]) -> Dict[str, str]:
+    """Start a CarolApp process.
+
+    Args:
+        app_info (Dict[str, str]): A dict containing the `app_name` and the\
+            `process_name`.
+
+    Raises:
+        TaskError: If task fail, raises: Something wrong while processing the\
+            task.
+
+    Returns:
+        Dict[str, str]: A dict with the `task_id` and the success status.
+    """
+    task_information = apps.start_app_process(
+        app_info.get("process_name"), app_info.get("app_name")
+    )
+
+    task_id = task_information.get("data").get("mdmId")
+    task_succeded = task_information.get("success")
+
+    if not task_succeded:
+        raise TaskError(task_id)
+
+    return {"task_id": task_id, "success": task_succeded}
+
+
+def get_task_status(task_id: str) -> str:
+    """Check task status by `task_id`.
+
+    Args:
+        task_id (str): The TaskID.
+
+    Returns:
+        str: The task status. Can be: READY, RUNNING, COMPLETED, FAILED or\
+            CANCELED.
+    """
+    return tasks.get_task(task_id).task_status
--- tests/test_carol_api.py
+++ tests/test_carol_api.py
@@ -1,8 +1,8 @@
-import carol_app_monitor.carol_api as monitor
+from carol_app_monitor import carol_api
 
 
 def test_get_properties_from_app_must_get_properties_from_all_existing_carol_apps():
-    properties = monitor.get_properties_from_app()
+    properties = carol_api.get_properties_from_app()
 
     assert len(properties.keys()) == 6
     assert properties.items() != {}
@@ -20,7 +20,7 @@ def test_get_all_carol_app_names_must_return_correctly_carol_app_names(
         "carolappmonitor",
     ]
 
-    carol_app_names = monitor.get_all_carol_app_names(properties)
+    carol_app_names = carol_api.get_all_carol_app_names(properties)
 
     assert expected_data == carol_app_names
 
@@ -28,7 +28,7 @@ def test_get_all_carol_app_names_must_return_correctly_carol_app_names(
 def test_get_process_status_must_return_correctly_carol_app_status_if_is_a_list_of_carol_apps():
     carol_apps = ["cargoappdemo", "priappdemo"]
 
-    carol_apps_status = monitor.get_process_status(carol_apps)
+    carol_apps_status = carol_api.get_process_status(carol_apps)
 
     assert carol_apps_status == {
         "cargoappdemo": "PAUSED",
@@ -39,8 +39,44 @@ def test_get_process_status_must_return_correctly_carol_app_status_if_is_a_list_
 def test_get_process_status_must_return_correctly_carol_app_status_if_is_a_unique_carol_app():
     carol_apps = "cargoappdemo"
 
-    carol_apps_status = monitor.get_process_status(carol_apps)
+    carol_apps_status = carol_api.get_process_status(carol_apps)
 
     assert carol_apps_status == {
         "cargoappdemo": "PAUSED",
     }
+
+
+def test_get_process_name_must_return_a_list_of_dict_containing_app_name_and_process_name_if_is_a_list_of_carol_apps():
+    carol_apps = ["cargoappdemo", "priappdemo"]
+
+    carol_apps_infos = carol_api.get_process_name(carol_apps)
+
+    assert carol_apps_infos == [
+        {"app_name": "cargoappdemo", "process_name": "cargoapp"},
+        {"app_name": "priappdemo", "process_name": "priapp"},
+    ]
+
+
+def test_get_process_name_must_return_a_list_of_dict_containing_app_name_and_process_name_if_is_a_unique_of_carol_app():
+    carol_apps = "cargoappdemo"
+
+    carol_apps_infos = carol_api.get_process_name(carol_apps)
+
+    assert carol_apps_infos == [
+        {"app_name": "cargoappdemo", "process_name": "cargoapp"}
+    ]
+
+
+def test_start_process_must_return_task_id_and_success_status():
+    app_info = {"app_name": "cargoappdemo", "process_name": "cargoapp"}
+
+    task_info = carol_api.start_app_process(app_info)
+
+    assert task_info.get("task_id") is not None
+    assert task_info.get("success") == True
+
+
+def test_get_task_status():
+    task_status = carol_api.get_task_status("2d7b84d6834d4616a27dbd8c781f0320")
+
+    assert task_status == "COMPLETED"
