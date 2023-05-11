545d2cd2bcad71ec693572cbaf0deaff30f74b89
RWallan <3am.richardwallan@gmail.com>
2023-05-10 22:50:58 -0300

    ✨Feature: MaxRetryError exception
    Excpetion to handling max retry errors.

--- carol_app_monitor/exceptions.py
+++ carol_app_monitor/exceptions.py
@@ -1,5 +1,5 @@
 class TaskError(Exception):
-    """Custom exception to handling task erros.
+    """Custom exception to handling task errors.
 
     Args:
         task_id (str): TaskID.
@@ -7,3 +7,14 @@ class TaskError(Exception):
 
     def __init__(self, task_id: str) -> None:
         self.message = f"Something wrong while processing the task: {task_id}"
+
+
+class MaxRetryError(Exception):
+    """Custom exception to handling max retry errors.
+
+    Args:
+        max_retries (int): Max retries.
+    """
+
+    def __init__(self, num_retries: int) -> None:
+        self.message = f"Exceeded maximum number of attempts ({num_retries})"
--- tests/test_exceptions.py
+++ tests/test_exceptions.py
@@ -11,3 +11,10 @@ def test_task_error_must_return_correct_message():
         excinfo.value.message
         == "Something wrong while processing the task: my_task_id"
     )
+
+
+def test_max_retry_error_must_return_correct_message():
+    with pytest.raises(exceptions.MaxRetryError) as excinfo:
+        raise exceptions.MaxRetryError(2)
+
+    assert excinfo.value.message == "Exceeded maximum number of attempts (2)"
