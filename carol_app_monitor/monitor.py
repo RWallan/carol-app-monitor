6aeb9bf70ed5bdab9862bee329c2dc2e0abc9479
RWallan <3am.richardwallan@gmail.com>
2023-05-11 12:30:18 -0300

    ✨Feature: Start all online process that's not running
    Function that filter online APPs from all CarolApps and check which isn't runnig.
    
    To this cases, the start_online_processes() function will try to restart the process for num_retries explicited.

--- carol_app_monitor/monitor.py
+++ carol_app_monitor/monitor.py
@@ -1,12 +1,17 @@
+import random
+import time
 from typing import Dict, List
 
 from .carol_api import (
+    check_task_status,
     get_all_carol_app_names,
     get_entity_type,
     get_process_name,
     get_process_status,
     get_properties_from_app,
+    start_app_process,
 )
+from .exceptions import MaxRetryError, TaskError
 from .utils import filter_dicts_by_values, merge_dicts
 
 
@@ -41,3 +46,89 @@ def create_monitor_schema() -> List[Dict[str, str]]:
     monitor_schema = filter_dicts_by_values(temp_schema, None, drop=True)
 
     return monitor_schema
+
+
+def start_online_processes(
+    monitor_schema: List[Dict[str, str]], num_retries: int = 2
+) -> Dict[str, Dict[str, str]]:
+    """Start all online processes that's not running.
+
+    This function will filter online APPs from all CarolApps and check which\
+    isn't running. To this cases, the `start_online_processes()` function\
+    will try to start the process for `num_retries` explicited.
+
+    Args:
+        monitor_schema (List[Dict[str, str]]): The list of dicts containing\
+        `app_name`, `process_status`, `process_name`, `entity_type`.
+
+        num_retries (int, optional): Number of retries that function will\
+            execute. Defaults to 2.
+
+    Returns:
+        Dict[str, Dict[str, str]]: The dictionary contaning the `app_name`,\
+        `status`, `success` and `detail`
+    """
+    online_processes = filter_dicts_by_values(
+        monitor_schema, "online", keep=True
+    )
+    not_running_processes = filter_dicts_by_values(
+        online_processes, "RUNNING", drop=True
+    )
+
+    processes_status = []
+    for process in not_running_processes:
+        retry = 1
+        app_name = process.get("app_name")
+        process_name = process.get("process_name")
+        try:
+            process_info = start_app_process(process)
+        except TaskError:
+            try:
+                time.sleep(round(12 * random.random() * 5, 2))
+                process_info = start_app_process(process)
+            except TaskError as error:
+                process_status = {
+                    "app_name": app_name,
+                    "status": {"success": "no", "detail": repr(error)},
+                }
+
+                processes_status.append(process_status)
+                break
+
+        task_name = f"AI Process: {process_name} START (Carol App: {app_name})"
+        task_info = check_task_status(process_info.get("task_id"), task_name)
+
+        if task_info.get("task_status") != "COMPLETED":
+            while retry <= num_retries:
+                process_info = start_app_process(process)
+
+                task_info = check_task_status(
+                    process_info.get("task_id"), task_name
+                )
+
+                if task_info.get("task_status") == "COMPLETED":
+                    break
+                else:
+                    time.sleep(round(12 * random.random() * 5, 2))
+                    retry += 1
+
+        if retry > num_retries:
+            error = MaxRetryError(num_retries)
+
+            process_status = {
+                "app_name": app_name,
+                "status": {"success": "no", "detail": repr(error)},
+            }
+
+            processes_status.append(process_status)
+
+            continue
+
+        process_status = {
+            "app_name": app_name,
+            "status": {"success": "yes", "detail": task_info.get("detail")},
+        }
+
+        processes_status.append(process_status)
+
+    return processes_status
--- tests/test_monitor.py
+++ tests/test_monitor.py
@@ -38,3 +38,67 @@ def test_create_monitor_schema_must_return_correct_schema():
     monitor_schema = monitor.create_monitor_schema()
 
     assert monitor_schema == expected_monitor_schema
+
+
+def test_start_online_processes_must_return_correct_infos():
+    monitor_schema = [
+        {
+            "app_name": "scloud1",
+            "process_status": "PAUSED",
+            "process_name": "docker_jupyter",
+            "entity_type": "batch",
+        },
+        {
+            "app_name": "teste",
+            "process_status": "PAUSED",
+            "process_name": "versao2",
+            "entity_type": "online",
+        },
+        {
+            "app_name": "cargoappdemo",
+            "process_status": "PAUSED",
+            "process_name": "cargoapp",
+            "entity_type": "batch",
+        },
+        {
+            "app_name": "aulas",
+            "process_status": "PAUSED",
+            "process_name": "cargoapp",
+            "entity_type": "batch",
+        },
+        {
+            "app_name": "priappdemo",
+            "process_status": "PAUSED",
+            "process_name": "priapp",
+            "entity_type": "online",
+        },
+    ]
+
+    num_retries = 1
+
+    expected_infos = [
+        {
+            "app_name": "teste",
+            "status": {
+                "success": "no",
+                "detail": "MaxRetryError('Exceeded maximum number of attempts (1)')",
+            },
+        },
+        {
+            "app_name": "priappdemo",
+            "status": {
+                "success": "yes",
+                "detail": "Task has successfully completed: AI Process: priapp START (Carol App: priappdemo)/<TASK_ID>",
+            },
+        },
+    ]
+
+    start_process_detail = monitor.start_online_processes(
+        monitor_schema, num_retries
+    )
+
+    assert start_process_detail[0] == expected_infos[0]
+    assert start_process_detail[1].get("status").get(
+        "success"
+    ) == expected_infos[1].get("status").get("success")
+    assert len(start_process_detail) == 2
